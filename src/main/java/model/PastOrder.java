1a7256a64cc174e7ed6e0c5e97fdd8a58cf6558a
Maxime <m.77.heurtevent@outlook.com>
2023-04-07 19:53:44 +0200

    🐛 Dates & order_history fixed

--- src/main/java/model/PastOrder.java
+++ src/main/java/model/PastOrder.java
@@ -7,15 +7,25 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class PastOrder{ //Rajouter ACCOMODATIONS
-    Hotels hotel = new Hotels();
+    String destination;
     LocalDate dateOfArrival, dateOfDeparture;
 
     public PastOrder() {
     }
 
-    public PastOrder(Hotels hotel, LocalDate dateOfArrival, LocalDate dateOfDeparture) {
-        this.hotel = hotel;
+    public PastOrder(String destination, LocalDate dateOfArrival, LocalDate dateOfDeparture) {
+        this.destination = destination;
         this.dateOfArrival = dateOfArrival;
         this.dateOfDeparture = dateOfDeparture;
+    }
+
+    public PastOrder(Document pastOrder){
+        this.destination = pastOrder.getString("destination");
+        this.dateOfArrival = pastOrder.getDate("date_of_arrival").toInstant().atZone(java.time.ZoneId.systemDefault()).toLocalDate();
+        this.dateOfDeparture = pastOrder.getDate("date_of_departure").toInstant().atZone(java.time.ZoneId.systemDefault()).toLocalDate();
+    }
+
+    public String toString(){
+        return "Destination: " + this.destination + " | Date of arrival: " + this.dateOfArrival + " | Date of departure: " + this.dateOfDeparture;
     }
 }
